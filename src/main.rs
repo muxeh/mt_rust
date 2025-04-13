21674cceb3df6ce0b9daa7c3bd8ced7bf845b44c
Tyler Rosselli <trossell@usc.edu>
2025-04-12 19:19:50 -0500

    I added the blockchain and also changed main to facilitate all of the transactions

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ mod network;
 mod transaction;
 mod wallet;
 use miner::mine_block;
+use crate::blockchain::Blockchain;
+
 
 fn main() {
     // Example setup
     let index = 0;
     let data = "Genesis Block".to_string();
     let previous_hash = "0".to_string();
-
     // Call miner to mine a new block
-    let mined_block = mine_block(index, data, previous_hash);
+    let genesis = mine_block(index, data, previous_hash);
+    let mut blockchain = Blockchain::new(genesis);
 
-    println!("🧱 Mined Block: {:#?}", mined_block);
+    println!("🧱 Added genesis to the blockchain: {:#?}", blockchain.latest());
 }
