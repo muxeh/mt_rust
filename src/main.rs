922fdea9428e30740f11cb0c7a8a8f3035662337
Tyler Rosselli <trossell@usc.edu>
2025-04-13 21:41:28 -0500

    Testing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod wallet;
 
 fn main() {
     println!("RustChain CLI running...");
-
+    //testing
     // Create a block
     let mut block = block::Block::new(0, "My Block".to_string(), "0x00".to_string());
     println!("My Block:\n{}", block);
